beed66334f7f1dc79a21507124d8fcce2d4337da
Ross Harrison <rtharrison86@gmail.com>
2016-06-27 23:55:37 -0400

    do all in one file

--- src/rharriso/Main.java
+++ src/rharriso/Main.java
@@ -9,11 +9,35 @@ import java.util.stream.Collectors;
 public class Main {
 
     private static Random rand = new Random();
+    static double appleCount;
 
     public static void main(String[] args) {
-        System.out.println("started");
+        int pow = Integer.parseInt(args[0]);
+        appleCount = Math.pow(2, pow);
+        System.out.printf("starting with set size (%d)\n", (int) appleCount);
+        testObjects();
+        testIntegers();
+        System.out.printf("finished with set size (%d)\n", (int) appleCount);
+    }
 
-        double appleCount = Math.pow(2, 25);
+    static void testIntegers() {
+        ArrayList<Integer> apples = new ArrayList<Integer>();
+        for(int i = 0; i < appleCount; i++){
+            apples.add(rand.nextInt(200));
+        }
+
+        long start0 = System.nanoTime();
+        long count0 = apples.parallelStream().filter(a -> a > 100).count();
+        long elapsed0 = System.nanoTime() - start0;
+
+        long start1 = System.nanoTime();
+        long count1 = apples.stream().filter(a -> a > 100).count();
+        long elapsed1 = System.nanoTime() - start1;
+
+        System.out.printf("Par-Int: %d \nSer-Int: %d\nDif-Int: %d\n", elapsed0, elapsed1, elapsed0 - elapsed1);
+    }
+
+    static void testObjects(){
         ArrayList<Apple> apples = new ArrayList<Apple>();
         for(int i = 0; i < appleCount; i++){
             Apple a = new Apple();
@@ -21,24 +45,15 @@ public class Main {
             apples.add(a);
         }
 
-        System.out.println("Constructed");
-        System.out.println();
-
-        System.out.println("Starting Parallel");
         long start0 = System.nanoTime();
         long count0 = apples.parallelStream().filter(a -> a.getWeight() > 100).count();
         long elapsed0 = System.nanoTime() - start0;
-        System.out.println("Parallel Done");
-        System.out.println();
 
-        System.out.println("Starting Serial");
         long start1 = System.nanoTime();
         long count1 = apples.stream().filter(a -> a.getWeight() > 100).count();
         long elapsed1 = System.nanoTime() - start1;
-        System.out.println("Serial Done");
-        System.out.println();
 
-        System.out.printf("Par %d \nSer %d\nDif %d\n", elapsed0, elapsed1, elapsed0 - elapsed1);
+        System.out.printf("Par-Obj %d \nSer-Obj %d\nDif-Obj %d\n", elapsed0, elapsed1, elapsed0 - elapsed1);
     }
 }
 
@@ -47,7 +62,6 @@ class Apple {
     public int getWeight() {
         return weight;
     }
-
     public void setWeight(int weight) {
         this.weight = weight;
     }
